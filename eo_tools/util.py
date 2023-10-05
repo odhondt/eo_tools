93f46aeb23ef10927072a3ca37440b8f2fdfbc67
Olivier D'Hondt <dhondt.olivier@gmail.com>
2023-10-05 12:31:57 +0000

    display configuration on map

--- eo_tools/util.py
+++ eo_tools/util.py
@@ -8,12 +8,12 @@ def visualize_S1_products_for_insar(product_geodataframe, aoi_geojson=None):
     """Visualize Sentinel-1 products on a map to choose pairs that are relevant for InSAR.
 
     Args:
-        product_geodataframe (geopandas GeoDataFrame): products returned by sentinelsat (see example notebook). 
+        product_geodataframe (geopandas GeoDataFrame): products returned by sentinelsat (see example notebook).
         aoi_geojson (dict, optional): Area of interest to display on the map. Defaults to None.
 
     Returns:
         folium Map: interactive map
-    """    
+    """
     map_ = folium.Map()
     grouped = product_geodataframe.groupby(["relativeorbitnumber", "slicenumber"])
     groups = list(grouped.groups.keys())
@@ -23,7 +23,6 @@ def visualize_S1_products_for_insar(product_geodataframe, aoi_geojson=None):
     for g in groups:
         # convert Multipolygons to Polygons
         dfg = grouped.get_group(g).explode(index_parts=False).to_crs("EPSG:4236")
-
         # find the intersection of all geometries
         inter = intersection_all(dfg.geometry)
 
@@ -35,11 +34,14 @@ def visualize_S1_products_for_insar(product_geodataframe, aoi_geojson=None):
         item["properties"] = {}
         if all(dfg.orbitdirection == "ASCENDING"):
             item["properties"]["color"] = "cadetblue"
+            orbit_conf = "Ascending"
         elif all(dfg.orbitdirection == "DESCENDING"):
             item["properties"]["color"] = "orange"
+            orbit_conf = "Descending"
         else:
             # for safety -- not sure if this may happen
             item["properties"]["color"] = "red"
+            orbit_conf = "Mix of ascending and descending"
         gj["features"].append(item)
         folium_products = folium.GeoJson(
             data=gj,
@@ -48,22 +50,24 @@ def visualize_S1_products_for_insar(product_geodataframe, aoi_geojson=None):
                 "color": x["properties"]["color"],
             },
         )
-    
+
         date_ts = dfg["beginposition"].dt.strftime("%Y-%m-%d %X")
         date_str = "<br>".join(
-            date_ts.index.astype(str).str.cat(date_ts.values, sep=": ")
+            date_ts.index.astype(str).str.cat(date_ts.values, sep=" / ")
         )
-        folium.Tooltip(f"index: date<br>{date_str}").add_to(folium_products)
+        folium.Tooltip(
+            f"<b>Configuration:</b><br>{orbit_conf}, Orbit: {g[0]}, Slice: {g[1]}<br><b>Product list (index / date):</b><br>{date_str}"
+        ).add_to(folium_products)
         folium_products.add_to(map_)
 
     if aoi_geojson is not None:
         folium_aoi = folium.GeoJson(
-                data=aoi_geojson,
-                style_function=lambda x: {
-                    "fillColor": "none",
-                    "color": "grey",
-                },
-            )
+            data=aoi_geojson,
+            style_function=lambda x: {
+                "fillColor": "none",
+                "color": "grey",
+            },
+        )
         folium.Tooltip(f"Area of Interest").add_to(folium_aoi)
         folium_aoi.add_to(map_)
     map_.fit_bounds(bbox)
