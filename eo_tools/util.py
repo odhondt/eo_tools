a8171e043e33f3f25e47ba80e867d308b11ac0d0
Olivier D'Hondt <dhondt.olivier@gmail.com>
2023-11-30 15:31:00 +0000

    single function that maps all product types

--- eo_tools/util.py
+++ eo_tools/util.py
@@ -5,6 +5,8 @@ from shapely.geometry import mapping
 
 import numpy as np
 import rasterio
+import geopandas as gpd
+import json
 from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.colors import to_hex
 
@@ -82,6 +84,64 @@ def visualize_S1_products_for_insar(product_geodataframe, aoi_geojson=None):
     return map_
 
 
+# check for geometrical overlap
+def has_overlap(geom1, geom2, tolerance=0.01):
+    intersection = geom1.intersection(geom2)
+    return intersection.area / min(geom1.area, geom2.area) >= (1 - tolerance)
+
+
+# make groups of almost fully overlapping products
+def group_by_overlap(dfg):
+    init_list = list(dfg.index)
+    groups = []
+    while len(init_list) > 0:
+        geom_a = dfg.geometry.loc[init_list[0]]
+        grp = []
+        grp.append(init_list.pop(0))
+        i = 0
+        while i < len(init_list):
+            geom_b = dfg.geometry.loc[init_list[i]]
+            if has_overlap(geom_a, geom_b):
+                grp.append(init_list.pop(i))
+            else:
+                i += 1
+        groups.append(grp)
+    return groups
+
+
+def explore_products(products):
+    # Convert results to geodataframe
+    gj = products.as_geojson_object()
+    gdf = gpd.read_file(json.dumps(gj))
+
+    ll = gdf.total_bounds.reshape(2, 2)
+    # folium inverts coordinates
+    bbox = list(map(lambda x: [x[1], x[0]], ll))
+
+    m = folium.Map()
+    by_orbit = gdf.groupby("relativeOrbitNumber")
+    group_indices = []
+    for idx in by_orbit.groups:
+        dfg = by_orbit.get_group(idx).explode(index_parts=False)
+
+        # group very overlapping products
+        group_indices = group_by_overlap(dfg)
+        for g in group_indices:
+            sel = gdf.loc[g]
+            folium_products = folium.GeoJson(sel.unary_union).add_to(m)
+            date_ts = sel["startTimeFromAscendingNode"].dt.strftime("%Y-%m-%d %X")
+            date_str = "<br>".join(
+                date_ts.index.astype(str).str.cat(date_ts.values, sep=" / ")
+            )
+            orbit_conf = sel.iloc[0]["orbitDirection"]
+            orbit_num = sel.iloc[0]["relativeOrbitNumber"]
+            folium.Tooltip(
+                f"<b>Configuration:</b><br>{orbit_conf}, Orbit: {orbit_num}<br><b>Product list (index / date):</b><br>{date_str}"
+            ).add_to(folium_products)
+    m.fit_bounds(bbox)
+    return m
+
+
 def visualize_insar_phase(file_in):
     """Visualize interferometric phase on a map with a cyclic colormap (similar to SNAP).
 
@@ -126,13 +186,13 @@ def visualize_insar_coh(file_in):
         folium.Map: raster visualization on an interactive map
     """
 
-
     client = TileClient(file_in)
     t = get_folium_tile_layer(client, vmin=0.0, vmax=1.0)
 
     m = folium.Map(location=client.center(), zoom_start=client.default_zoom)
     t.add_to(m)
     return m
+
 
 def visualize_sar_intensity(file_in, vmin=None, vmax=None):
     """Visualize intensity on a map.
@@ -145,7 +205,7 @@ def visualize_sar_intensity(file_in, vmin=None, vmax=None):
     """
 
     try:
-        with rasterio.open(file_in) as src: 
+        with rasterio.open(file_in) as src:
             mean_val = src.tags()["mean_value"]
     except:
         raise Exception("File not found or no 'mean_value' tag.")
@@ -157,7 +217,7 @@ def visualize_sar_intensity(file_in, vmin=None, vmax=None):
         vmin_ = vmin
 
     if vmax is None:
-        vmax_ = 2.5*float(mean_val)
+        vmax_ = 2.5 * float(mean_val)
     else:
         vmax_ = vmax
 
