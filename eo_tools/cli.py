9afcc6e54bf02241cd706bc64d99d9648bcf1b7e
Olivier D'Hondt <dhondt.olivier@gmail.com>
2023-12-05 10:51:02 +0000

    remove unused files

--- eo_tools/cli.py
+++
@@ -1,9 +0,0 @@
-import click
-from .processor import process
-
-@click.command()
-@click.option('--config-file', '-c', required=False, type=click.Path(),
-              help='Full path to an INI-style configuration text file.')
-
-def cli(config_file):
-    process(config_file=config_file)
