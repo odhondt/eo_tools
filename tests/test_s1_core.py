65bf0350ab652913fb56cd0e28172a84d40f0eba
Olivier D'Hondt <dhondt.olivier@gmail.com>
2024-05-21 09:35:43 +0000

    use fixture in test

--- tests/test_s1_core.py
+++ tests/test_s1_core.py
@@ -1,25 +1,23 @@
 import pytest
-from pathlib import Path
-import os
-import numpy as np
 from eo_tools.S1.core import S1IWSwath
 
 
-def test_s1iwswath_init():
-    import os
-    # print(os.get_cwd())
+@pytest.fixture
+def create_swath():
     safe_dir = "./data/S1A_IW_SLC__1SDV_20230904T063730_20230904T063757_050174_0609E3_DAA1.SAFE"
     iw = 1
     pol = "vh"
+    return S1IWSwath(safe_dir, iw, pol)
 
-    # Initialize the S1IWSwath object
-    swath = S1IWSwath(safe_dir, iw, pol)
+def test_s1iwswath_init(create_swath):
+    import os
 
-    # Assertions
+    swath = create_swath
+
     assert os.path.isfile(swath.pth_tiff)
     assert (
         swath.start_time == "2023-09-04T06:37:31.072288"
-    )  # Replace with actual start time from your metadata
+    )
     assert swath.lines_per_burst == 1507
     assert swath.samples_per_burst == 23055
     assert swath.burst_count == 9
