b1c358229ef49e6630e6f7ffb86855155d9c8860
Olivier D'Hondt <dhondt.olivier@gmail.com>
2024-08-21 14:51:35 +0000

    remove unused test

--- tests/test_s1_process.py
+++ tests/test_s1_process.py
@@ -4,7 +4,7 @@ import tempfile
 import os
 import numpy as np
 import xarray as xr
-from eo_tools.S1.process import coherence, _merge_luts
+from eo_tools.S1.process import coherence
 
 
 # TODO create dataArrays instead of datasets
@@ -29,14 +29,3 @@ def test_coherence(create_test_data):
     prm_file, sec_file, out_file = create_test_data
     coherence(prm_file, sec_file, out_file)
     assert os.path.exists(out_file)
-
-
-def test_merge_luts():
-    lines = 1507
-    overlap = 165
-    luts = ["./data/lut_5.tif", "./data/lut_6.tif"]
-    fout = "/tmp/merged.tif"
-    _merge_luts(luts, fout, lines, overlap, 4)
-    da = xr.open_dataset(fout)["band_data"]
-    assert da.dims == ("band", "y", "x")
-    assert da.shape == (2, 3328, 6773)
