12134a3ccbb5b2a722af7eb1526c83698b36ab50
Olivier D'Hondt <dhondt.olivier@gmail.com>
2024-08-21 17:03:25 +0000

    testing processor with mock s1 data

--- tests/test_s1_process.py
+++ tests/test_s1_process.py
@@ -1,10 +1,12 @@
-from unittest.mock import patch, MagicMock
 import pytest
 import tempfile
 import os
 import numpy as np
 import xarray as xr
-from eo_tools.S1.process import coherence
+from eo_tools.S1.process import coherence, process_insar
+import geopandas as gpd
+import multiprocessing
+multiprocessing.set_start_method('forkserver', force=True)
 
 
 # TODO create dataArrays instead of datasets
@@ -29,3 +31,44 @@ def test_coherence(create_test_data):
     prm_file, sec_file, out_file = create_test_data
     coherence(prm_file, sec_file, out_file)
     assert os.path.exists(out_file)
+
+def test_process_insar(tmp_path):
+
+    data_dir = "./data/S1"
+    file_aoi = "/eo_tools/data/Morocco_tiny.geojson"
+    shp = gpd.read_file(file_aoi).geometry[0]
+    ids = [
+        "S1A_IW_SLC__1SDV_20230904T063730_20230904T063757_050174_0609E3_DAA1",
+        "S1A_IW_SLC__1SDV_20230916T063730_20230916T063757_050349_060FCD_6814",
+    ]
+    primary_dir = f"{data_dir}/{ids[0]}.SAFE"
+    secondary_dir = f"{data_dir}/{ids[1]}.SAFE"
+    process_args = dict(
+        dir_prm=primary_dir,
+        dir_sec=secondary_dir,
+        outputs_prefix=tmp_path,
+        aoi_name=None,
+        shp=shp,
+        pol="vv",
+        subswaths=["IW1", "IW2", "IW3"],
+        write_coherence=True,
+        write_interferogram=True,
+        write_primary_amplitude=True,
+        write_secondary_amplitude=False,
+        apply_fast_esd=True,
+        dir_dem="./data/dem/",
+        dem_upsampling=0.5,
+        dem_force_download=False,
+        dem_buffer_arc_sec=20,
+        boxcar_coherence=[3, 10],
+        filter_ifg=True,
+        multilook=[2, 8],
+        # multilook=[4, 16],
+        warp_kernel="nearest",
+        # warp_kernel="bicubic",
+        clip_to_shape=True,
+    )
+    out_dir = process_insar(
+        **process_args 
+    )
+    assert out_dir
