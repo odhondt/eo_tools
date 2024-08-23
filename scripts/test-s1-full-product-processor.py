337e45c37c847018e84b7f082b3c684ad11ad84f
Olivier D'Hondt <dhondt.olivier@gmail.com>
2024-08-23 13:40:54 +0000

    Improved insar test script
    - fires up a browser with the InSAR outputs
    - compares ours to reference data made with SNAP

--- scripts/test-s1-full-product-processor.py
+++ scripts/test-s1-full-product-processor.py
@@ -2,6 +2,8 @@
 import logging
 
 logging.basicConfig(level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("numexpr").setLevel(logging.WARNING)
 
 # Uncomment these lines to test conda imports
 # import sys
@@ -13,16 +15,19 @@ logging.basicConfig(level=logging.INFO)
 # from folium import LayerControl
 import os
 import geopandas as gpd
-from eodag import EODataAccessGateway
+# from eodag import EODataAccessGateway
 from eo_tools.S1.process import process_insar
 from eo_tools.auxils import remove
+from eo_tools_dev.util import show_cog, serve_map, palette_phi
+from math import pi
+import folium
+from folium import LayerControl
 
 # credentials need to be stored in the following file (see EODAG docs)
-confpath = "/data/eodag_config.yml"
-dag = EODataAccessGateway(user_conf_file_path=confpath)
+# confpath = "/data/eodag_config.yml"
+# dag = EODataAccessGateway(user_conf_file_path=confpath)
 # make sure cop_dataspace will be used
-dag.set_preferred_provider("cop_dataspace")
-logging.basicConfig(level=logging.INFO)
+# dag.set_preferred_provider("cop_dataspace")
 
 # %%
 data_dir = "/data/S1"
@@ -37,16 +42,16 @@ outputs_prefix = "/data/res/test-full-processor"
 
 # %%
 # load a geometry
-file_aoi = "/eo_tools/data/Morocco_tiny.geojson"
-# file_aoi = "/eo_tools/data/Morocco_AOI.geojson"
+# file_aoi = "/eo_tools/data/Morocco_tiny.geojson"
+file_aoi = "/eo_tools/data/Morocco_AOI.geojson"
 shp = gpd.read_file(file_aoi).geometry[0]
 
-search_criteria = {
-    "productType": "S1_SAR_SLC",
-    "start": "2023-09-03",
-    "end": "2023-09-17",
-    "geom": shp,
-}
+# search_criteria = {
+#     "productType": "S1_SAR_SLC",
+#     "start": "2023-09-03",
+#     "end": "2023-09-17",
+#     "geom": shp,
+# }
 
 # uncomment if files are not already on disk
 # results, _ = dag.search(**search_criteria)
@@ -68,10 +73,11 @@ process_args = dict(
     shp=shp,
     pol="vv",
     subswaths=["IW1", "IW2", "IW3"],
+    # subswaths="IW1",
     write_coherence=True,
     write_interferogram=True,
-    write_primary_amplitude=True,
-    write_secondary_amplitude=True,
+    write_primary_amplitude=False,
+    write_secondary_amplitude=False,
     apply_fast_esd=True,
     dem_upsampling=1.8,
     dem_force_download=True,
@@ -81,52 +87,23 @@ process_args = dict(
     multilook=[1, 4],
     warp_kernel="bicubic",
     clip_to_shape=True,
+    # skip_preprocessing=True,
 )
 
-out_dir = process_insar(
-   **process_args 
-)
+out_dir = process_insar(**process_args)
+
 
 # %%
-# Optional test
-check_outputs = True
-if check_outputs:
-    import rioxarray as riox
-    from xarray.testing import assert_allclose
-    log = logging.getLogger(__name__)
-    test_args = dict(
-        dir_prm=primary_dir,
-        dir_sec=secondary_dir,
-        outputs_prefix=outputs_prefix,
-        aoi_name=None,
-        shp=shp,
-        pol="vv",
-        subswaths=["IW1", "IW2", "IW3"],
-        write_coherence=True,
-        write_interferogram=True,
-        write_primary_amplitude=True,
-        write_secondary_amplitude=True,
-        apply_fast_esd=True,
-        dem_upsampling=1.8,
-        dem_force_download=True,
-        dem_buffer_arc_sec=40,
-        boxcar_coherence=[3, 10],
-        filter_ifg=True,
-        multilook=[1, 4],
-        warp_kernel="bicubic",
-        clip_to_shape=True,
-    )
+# compare with reference data processed with SNAP
+out_dir = "/data/res/test-full-processor/S1_InSAR_2023-09-04-063730__2023-09-16-063730"
+ref_dir = "/data/reference/S1_InSAR_VV_2023-09-04-063730__2023-09-16-063730_Morocco"
 
-    if test_args != process_args or file_aoi != "/eo_tools/data/Morocco_tiny.geojson":
-        raise ValueError("Wrong setup for output checking")
-    else:
-        log.info("Checking output validity")
-        dir_ref = "/eo_tools/data/test-full-processor/S1_InSAR_2023-09-04-063730__2023-09-16-063730"
-        for var in ["phi", "coh", "amp_prm", "amp_sec"]:
-            da_ref = riox.open_rasterio(f"{dir_ref}/{var}_vv.tif", masked=True)
-            da_out = riox.open_rasterio(f"{out_dir_prev}/{var}_vv.tif", masked=True)
-            try:
-                assert_allclose(da_ref, da_out)
-                log.info(f"{var} outputs are OK")
-            except:
-                raise RuntimeError(f"Variable {var} deviates from reference")
+m = folium.Map()
+_ = show_cog(f"{ref_dir}/phi.tif", m, rescale=f"{-pi},{pi}", colormap=palette_phi())
+_ = show_cog(f"{out_dir}/phi_vv.tif", m, rescale=f"{-pi},{pi}", colormap=palette_phi())
+_ = show_cog(f"{ref_dir}/coh.tif", m, rescale=f"0,1")
+_ = show_cog(f"{out_dir}/coh_vv.tif", m, rescale=f"0,1")
+LayerControl().add_to(m)
+
+# open in a browser
+serve_map(m)
