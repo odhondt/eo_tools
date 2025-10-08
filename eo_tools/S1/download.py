20c3be9703a4caa2a4881888cf331b95f0090294
Olivier D'Hondt <dhondt.olivier@gmail.com>
2025-10-08 14:30:39 +0000

    adapt fields for `util.explore_products`

--- eo_tools/S1/download.py
+++ eo_tools/S1/download.py
@@ -8,6 +8,7 @@ from dask.diagnostics import ProgressBar
 from pathlib import Path
 from urllib.parse import urlparse
 import os
+from datetime import datetime
 
 from rasterio.session import AWSSession
 import rasterio
@@ -15,6 +16,46 @@ import boto3
 
 from eo_tools.auxils import get_burst_geometry
 from eo_tools.S1.core import read_metadata
+
+
+# search with pystac client and store in a dataframe
+# TODO: chack arg validity ()
+def search_products(**kwargs):
+    # Search using STAC api
+    catalog = Client.open("https://stac.dataspace.copernicus.eu/v1/")
+    search = catalog.search(**kwargs)  # , ids=ids)
+    items = list(search.items())
+    from shapely.geometry import shape
+
+    start_times = []
+    ids = []
+    relative_orbits = []
+    orbit_states = []
+    geometries = []
+    items_ = [it.to_dict() for it in items]
+
+    for it in items:
+        it_dict = it.to_dict()
+        props = it_dict["properties"]
+        start_times.append(datetime.fromisoformat(props["start_datetime"]))
+        ids.append(it.id)
+        relative_orbits.append(props["sat:relative_orbit"])
+        orbit_states.append(props["sat:orbit_state"])
+        geometries.append(shape(it.geometry))
+
+    # gdf = gpd.GeoDataFrame(data={"id": ids, "start_time": start_times, "orbit": relative_orbits, "orbit_state": orbit_states, "stac_item": items_}, geometry=geometries)
+    # use columns compatible with util.explore_products
+    gdf = gpd.GeoDataFrame(
+        data={
+            "id": ids,
+            "startTimeFromAscendingNode": start_times,
+            "relativeOrbitNumber": relative_orbits,
+            "orbitDirection": orbit_states,
+            "stac_item": items_,
+        },
+        geometry=geometries,
+    )
+    return gdf
 
 
 def download_partial_products(products, shp, out_dir, aws_key, aws_secret):
@@ -99,10 +140,6 @@ def download_partial_products(products, shp, out_dir, aws_key, aws_secret):
         # find what subswaths and bursts intersect AOI
         gdf_burst = gdf_burst[gdf_burst.intersects(shp)]
 
-        # TODO: loop on polarizations and iw
-        iw = "3"
-        pol = "vv"
-
         if gdf_burst.empty:
             raise RuntimeError(
                 "The list of bursts to process is empty. Make sure shp intersects the product."
@@ -119,7 +156,7 @@ def download_partial_products(products, shp, out_dir, aws_key, aws_secret):
                 meta = read_metadata(pth_xml=pth_xml)
                 burst_info = meta["product"]["swathTiming"]
                 lines_per_burst = int(burst_info["linesPerBurst"])
-                burst_indices = gdf_burst[gdf_burst.subswath == f"IW{iw}"].burst
+                burst_indices = gdf_burst[gdf_burst.subswath == subswath].burst
                 min_burst = burst_indices.min()
                 max_burst = burst_indices.max()
                 line_start = lines_per_burst * (min_burst - 1)
@@ -127,7 +164,7 @@ def download_partial_products(products, shp, out_dir, aws_key, aws_secret):
                 line_end = line_start + num_lines
 
                 # open raster
-                url = it.assets[f"iw{iw}-{pol}"].href
+                url = it.assets[f"{subswath.lower()}-{pol}"].href
                 # !! Important: open as a dataset (band_as_variable=True)
                 with rasterio.Env(session=rio_session, AWS_VIRTUAL_HOSTING=False):
                     ds = riox.open_rasterio(url, chunks="auto", band_as_variable=True)
