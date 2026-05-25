c109442f1800ef39bfb063311ab7fe7dcb117268
Olivier D'Hondt <dhondt.olivier@gmail.com>
2026-05-25 13:45:28 +0000

    display aois and partial product for dev

--- scripts/show_all_s1_on_map.py
+++ scripts/show_all_s1_on_map.py
@@ -1,5 +1,6 @@
 # %%
 from folium import Map, GeoJson, Tooltip, LayerControl, Popup
+import json
 import zipfile
 from pathlib import Path
 from shapely.geometry import Polygon, mapping
@@ -7,6 +8,7 @@ from eo_tools_dev.util import serve_map
 
 # change to you directory
 data_dir = "/data/S1"
+aoi_dir = Path(__file__).resolve().parent / "../data"
 
 
 def extract_footprint(manifest):
@@ -32,6 +34,53 @@ def extract_footprint(manifest):
         raise RuntimeError("No footprint found!")
 
 
+def add_geojson_files_to_map(m, folder):
+    folder = Path(folder)
+    if not folder.exists():
+        return
+
+    for geojson_file in sorted(folder.glob("*.geojson")):
+        with geojson_file.open() as file:
+            geojson_data = json.load(file)
+
+        gj = GeoJson(
+            geojson_data,
+            name=geojson_file.name,
+            style_function=lambda feature: {
+                "fillColor": "black",
+                "color": "black",
+                "weight": 2,
+                "fillOpacity": 0.0,
+            },
+        )
+        gj.add_child(Popup(f"Filename: {geojson_file.name}"))
+        gj.add_to(m)
+
+
+def add_partial_products_to_map(m, folder):
+    folder = Path(folder)
+    if not folder.exists():
+        return
+
+    for f in list(folder.glob("*.SAFE")):
+        pth = zipfile.Path(f) if Path(f).suffix == ".zip" else Path(f)
+        manifest = list(pth.glob("**/manifest.safe"))[0]
+        footprint = extract_footprint(manifest)
+
+        gj = GeoJson(
+            mapping(footprint),
+            name=f"partial {f.stem}",
+            style_function=lambda feature: {
+                "fillColor": "pink",
+                "color": "hotpink",
+                "weight": 2,
+                "fillOpacity": 0.15,
+            },
+        )
+        gj.add_child(Popup(f"Product ID: partial {f.stem}<br>Path: {str(f)}"))
+        gj.add_to(m)
+
+
 m = Map()
 for f in list(Path(data_dir).glob("S1*")):
     pth = zipfile.Path(f) if Path(f).suffix == ".zip" else Path(f)
@@ -41,6 +90,9 @@ for f in list(Path(data_dir).glob("S1*")):
     gj = GeoJson(mapping(footprint), name=f.stem)
     gj.add_child(Popup(f"Product ID: {f.stem}<br>Path: {str(f)}"))
     gj.add_to(m)
+
+add_partial_products_to_map(m, Path(data_dir) / "partial_dls")
+add_geojson_files_to_map(m, aoi_dir)
 
 LayerControl().add_to(m)
 serve_map(m)
